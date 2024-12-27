333245851a580c03194a4ec088f4e1071ace5313
Fatma R <anthoschichequs@gmail.com>
2024-12-27 12:35:43 +0300

    nav uzerinden acilan sayfalar duzenlendi ve gerekli dosyalar eklendi. Hata duzeltildi.

--- public/noteSite/about.view.php
+++ public/noteSite/about.view.php
@@ -18,10 +18,9 @@
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <a href="index.php" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                            <a href="about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About Us</a>
+                            <a href="index.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
+                            <a href="about.php" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">About Us</a>
                             <a href="contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
-
                         </div>
                     </div>
                 </div>
@@ -120,12 +119,12 @@
 
     <header class="bg-white shadow">
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home page</h1>
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">About Us</h1>
         </div>
     </header>
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <p>Hello.  Welcome to the Home Page.</p>
+            <p>You are in about us page.</p>
         </div>
     </main>
 </div>

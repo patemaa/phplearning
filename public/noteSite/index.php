2a1e4d3e9f6a6e27b533d256b11573cca7d946ad
Fatma R <anthoschichequs@gmail.com>
2024-12-30 11:14:06 +0300

    functions dosyasi eklendi ve dinamiklik icin daha fazla duzenleme yapildi.

--- public/noteSite/index.php
+++ public/noteSite/index.php
@@ -1,14 +1,8 @@
 <?php
 
+require "functions.php";
+
 $heading = 'Home Page';
-
-function dd($value){
-    echo "<pre>";
-    var_dump($value);
-    echo "</pre>";
-
-    die();
-}
 
 //if ($_SERVER["REQUEST_URI"]=== '/') {
 //    echo 'bg-gray-900 text-white';
@@ -17,8 +11,6 @@ function dd($value){
 //}
 
 //echo $_SERVER['$REQUEST_URI'] === '/' ? 'bg-gray-900 text-white' : 'text-gray-300';
-
-
 
 require "views/index.view.php";
 

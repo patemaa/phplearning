d3bb9db0b6b5f4047d69371d5e56d4dc9fe95544
Fatma R <anthoschichequs@gmail.com>
2024-12-30 11:01:51 +0300

    nav kisminda dinamiklik icin kod degistirildi. dd() fonksiyonu olusturuldu.

--- public/noteSite/index.php
+++ public/noteSite/index.php
@@ -2,5 +2,23 @@
 
 $heading = 'Home Page';
 
+function dd($value){
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+
+//if ($_SERVER["REQUEST_URI"]=== '/') {
+//    echo 'bg-gray-900 text-white';
+//} else {
+//    echo 'text-gray-300';
+//}
+
+//echo $_SERVER['$REQUEST_URI'] === '/' ? 'bg-gray-900 text-white' : 'text-gray-300';
+
+
+
 require "views/index.view.php";
 
--- public/noteSite/views/partials/nav.php
+++ public/noteSite/views/partials/nav.php
@@ -8,9 +8,9 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="index.php" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                        <a href="about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About Us</a>
-                        <a href="contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                        <a href="/" class="<?= $_SERVER['REQUEST_URI'] === '/' ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sam font-medium">Home</a>
+                        <a href="about.php" class="<?= $_SERVER['REQUEST_URI'] === '/about.php' ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sam font-medium">About Us</a>
+                        <a href="contact.php" class="<?= $_SERVER['REQUEST_URI'] === '/contact.php' ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sam font-medium" >Contact</a>
 
 
                     </div>

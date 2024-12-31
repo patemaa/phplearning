a611426a4f00ea986c20d23d25c97a573b5812d3
Fatma R <anthoschichequs@gmail.com>
2024-12-31 15:50:59 +0300

    index.php'de degisiklik yapildi.

--- public/noteSite/index.php
+++ public/noteSite/index.php
@@ -1,4 +1,29 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+//connect to our MySql database
+
+class Person {
+    public $name;
+    public $age;
+
+    public function breathe ()
+    {
+//        echo "breathing";
+          echo $this->name . ' ' . "breathing";
+    }
+}
+
+$person = new Person();
+
+$person ->name = 'John Doe';
+$person ->age = 25;
+
+//dd($person);
+//dd($person->name);
+//dd($person->age);
+//dd($person->breathe());
+
+

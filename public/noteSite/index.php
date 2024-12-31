59bab6aa81c9ac149efdb435949debccbcac1b1b
Fatma R <anthoschichequs@gmail.com>
2024-12-31 16:38:15 +0300

    Database.php dosyasi olustutuldu ve database ayarlamalari yapildi.

--- public/noteSite/index.php
+++ public/noteSite/index.php
@@ -1,20 +1,10 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
 //require 'router.php';
 
-//connect to our MySql database\ and execute a query
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$dsn = 'mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4';
-
-$pdo = new PDO($dsn, 'root');
-
-$statement = $pdo -> prepare("select * from posts");
-
-$statement ->execute();
-
-$posts = $statement -> fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-    echo "<l'>" . $post['title'] . "</l>";
-}
+dd($posts);

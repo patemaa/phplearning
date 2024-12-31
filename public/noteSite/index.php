d2170397d19853419a0419ecd220075c260f9313
Fatma R <anthoschichequs@gmail.com>
2024-12-31 18:20:15 +0300

    Database.php ve index.php dosyalari icerisinde degisiklik yapildi. config.php dosyasi olusturuldu ve database islemlerinde dinamiklik saglandi.

--- public/noteSite/index.php
+++ public/noteSite/index.php
@@ -2,9 +2,12 @@
 
 require 'functions.php';
 require 'Database.php';
+
+$config = require 'config.php';
+
 //require 'router.php';
 
-$db = new Database();
+$db = new Database($config['database']);
 $posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
 dd($posts);

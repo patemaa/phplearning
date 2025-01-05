fd964c11a8dc6664c9b6e1b486336685f97c6f3d
Fatma R <anthoschichequs@gmail.com>
2025-01-05 11:10:45 +0300

    Hatalar giderildi ve nac dropdown menusu kaldirldi. Gereksiz dosyalar silindi.

--- public/noteSite/index.php
+++ public/noteSite/index.php
@@ -1,14 +1,16 @@
 <?php
 
 require 'functions.php';
+require "router.php";
 require 'Database.php';
 
 $config = require 'config.php';
 $db = new Database($config['database']);
 
 //$query = "SELECT * FROM posts WHERE id = {$id}";
-$id = $_GET['id'];
-$query = "SELECT * FROM posts WHERE id = :id";
+//$id = ($_GET['id']);
+//$query = "SELECT * FROM posts WHERE id = :id";
+//
+//$posts = $db->query($query, [':id' =>$id])->fetch();
 
-$posts = $db->query($query, [':id' => $id])->fetch();
 
--- public/noteSite/views/partials/nav.php
+++ public/noteSite/views/partials/nav.php
@@ -16,7 +16,7 @@
 
                     </div>
                 </div>
-            </div>
+            </div
             <div class="hidden md:block">
                 <div class="ml-4 flex items-center md:ml-6">
                     <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -27,35 +27,8 @@
                         </svg>
                     </button>
 
-                    <!-- Profile dropdown -->
-                    <div class="relative ml-3">
-                        <div>
-                            <button type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                <span class="absolute -inset-1.5"></span>
-                                <span class="sr-only">Open user menu</span>
-                                <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                            </button>
-                        </div>
 
-                        <!--
-                          Dropdown menu, show/hide based on menu state.
 
-                          Entering: "transition ease-out duration-100"
-                            From: "transform opacity-0 scale-95"
-                            To: "transform opacity-100 scale-100"
-                          Leaving: "transition ease-in duration-75"
-                            From: "transform opacity-100 scale-100"
-                            To: "transform opacity-0 scale-95"
-                        -->
-                        <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                            <!-- Active: "bg-gray-100 outline-none", Not Active: "" -->
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="-mr-2 flex md:hidden">
                 <!-- Mobile menu button -->
                 <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-controls="mobile-menu" aria-expanded="false">

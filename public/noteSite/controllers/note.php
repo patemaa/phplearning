d66b9a874b648fd8a867ae5faf295adff118357b
Fatma R <anthoschichequs@gmail.com>
2025-01-05 12:59:31 +0300

    authorization icin simdilik user_id manuel olarak girildi.

--- public/noteSite/controllers/note.php
+++ public/noteSite/controllers/note.php
@@ -5,7 +5,14 @@ $db = new Database($config['database']);
 
 $heading = 'Note';
 
-$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('select * from notes where user_id = :user and id = :id',
+    [
+        'user' => 3,
+        'id' => $_GET['id']
+    ])->fetch();
+if (!$note) {
+    abort();
+}
 
 require "views/note.view.php";
 

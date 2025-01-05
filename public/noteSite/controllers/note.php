0026c75aba8c5e137f19ca4c045d4eb41b7c8fd7
Fatma R <anthoschichequs@gmail.com>
2025-01-05 13:52:00 +0300

    authorization icin degisiklikler yapildi, authorize methodu eklendi.

--- public/noteSite/controllers/note.php
+++ public/noteSite/controllers/note.php
@@ -6,15 +6,9 @@ $db = new Database($config['database']);
 $heading = 'Note';
 $currentUserId = 3;
 
-$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
 
-if (!$note) {
-    abort();
-}
-
-if ($note['user_id'] !== $currentUserId) {
-    abort(Response::FORBIDDEN);
-}
+authorize($note['user_id'] === $currentUserId);
 
 require "views/note.view.php";
 
--- public/noteSite/controllers/notes.php
+++ public/noteSite/controllers/notes.php
@@ -5,7 +5,7 @@ $db = new Database($config['database']);
 
 $heading = 'My Notes';
 
-$notes = $db->query('select * from notes where user_id = 3')->fetchAll();
+$notes = $db->query('select * from notes where user_id = 3')->get();
 
 require "views/notes.view.php";
 

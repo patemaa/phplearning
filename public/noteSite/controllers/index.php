75f00e4da11359bdf5e23a8ddb6881570ee86d9a
Fatma R <anthoschichequs@gmail.com>
2025-01-07 10:26:12 +0300

    dinamiklik icin degisiklikler yapildi. pCore klasoru olusturuldu ve bazi dosyalarin yerleri degistirildi. kodda kolaylik icin methodlar olusturuldu.

--- public/noteSite/controllers/index.php
+++ public/noteSite/controllers/index.php
@@ -1,14 +1,5 @@
 <?php
 
-$heading = 'Home Page';
-
-//if ($_SERVER["REQUEST_URI"]=== '/') {
-//    echo 'bg-gray-900 text-white';
-//} else {
-//    echo 'text-gray-300';
-//}
-
-//echo $_SERVER['$REQUEST_URI'] === '/' ? 'bg-gray-900 text-white' : 'text-gray-300';
-
-require ("views/index.view.php");
-
+view("index.view.php", [
+    'heading' => 'Home',
+]);

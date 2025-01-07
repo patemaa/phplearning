57695be1d1e1dc73045e461c222d2bc5945b6269
Fatma R <anthoschichequs@gmail.com>
2025-01-07 16:11:41 +0300

    register sayfasi eklendi ve gerekli dosyalar olusturuldu. navbar'a register butonu eklendi.

---
+++ public/noteSite/controllers/registration/store.php
@@ -0,0 +1,46 @@
+<?php
+
+use Core\App;
+use Core\Validator;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$errors = [];
+if (!Validator::email($email)) {
+    $errors['email'] = 'Please provide a valid email address';
+}
+
+if (!Validator::string($password, 7, 225)) {
+    $errors['password'] = 'Please provide a passwrod at least seven characters';
+}
+
+if (!empty($errors)) {
+    return view('registration/create.view.php', [
+        'errors' => $errors
+    ]);
+}
+
+$user = $db->query('select * from users where email = :email', [
+    'email' => $email
+])->find();
+
+if ($user) {
+    header('location: /');
+    exit();
+} else {
+    $db->query('inseert into users(email,password) values(:email, :password)', [
+        'email' => $email,
+        'password' => $password
+    ]);
+
+    $_SESSION['user'] = [
+        'email' => $email
+    ];
+
+    header('location: /');
+    exit();
+}

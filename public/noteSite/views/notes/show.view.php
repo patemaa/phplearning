08ec7249e40650bfaab2f73e1723901b8526e5f0
Fatma R <anthoschichequs@gmail.com>
2025-01-07 15:41:26 +0300

    not editleme ve update etme eklendi.

--- public/noteSite/views/notes/show.view.php
+++ public/noteSite/views/notes/show.view.php
@@ -5,11 +5,10 @@
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <p><?= htmlspecialchars($note['body']) ?></p>
-            <form class="mt-6" method="POST">
-                <input type="hidden" name="_method" value="DELETE">
-                <input type="hidden" name="id" value="<?= $note['id'] ?>">
-                <button class="text-sm text-red-500">Delete</button>
-            </form>
+            <footer class="mt-6">
+                <a href="/note/edit?id=<?= $note['id'] ?>"
+                   class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Edit</a>
+            </footer>
             <p class="mt-6 text-purple-700 hover:text-blue-500 hover:underline">
                 <a href="/notes">Go Back</a>
             </p>

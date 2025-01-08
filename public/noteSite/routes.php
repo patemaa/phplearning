f164ad7bab1a7d1eb51f0f1608daae0cc9e65599
Fatma R <anthoschichequs@gmail.com>
2025-01-08 11:02:09 +0300

    Middleware klasoru olusturuldu ve gerekli dosyalar eklendi. notes sayfasinin sadece giris yapan kullanicilar tarafindan gorulmesi icin degisiklikler yapildi.

--- public/noteSite/routes.php
+++ public/noteSite/routes.php
@@ -4,10 +4,9 @@ $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
-
-$router->delete('/note', 'controllers/notes/destroy.php');
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
 $router->get('/note', 'controllers/notes/show.php');
+$router->delete('/note', 'controllers/notes/destroy.php');
 
 $router->get('/note/edit', 'controllers/notes/edit.php');
 $router->patch('/note', 'controllers/notes/update.php');
@@ -15,6 +14,5 @@ $router->patch('/note', 'controllers/notes/update.php');
 $router->get('/notes/create', 'controllers/notes/create.php');
 $router->post('/notes', 'controllers/notes/store.php');
 
-$router->get('/register', 'controllers/registration/create.php');
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
-
